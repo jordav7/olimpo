54cbb11f3a37c109b38cffe7244f3bcca18613b1
David Cruz <jordav_7@hotmail.com>
2018-09-06 07:03:40 -0500

    Menu ubmenu

--- src/app/components/sidebar/submenu/submenu.component.ts
+++ src/app/components/sidebar/submenu/submenu.component.ts
@@ -1,9 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, state, group } from '@angular/animations';
 
 @Component({
     // tslint:disable-next-line:component-selector
     selector: '[lec-submenu]',
-    templateUrl: 'submenu.component.html'
+    templateUrl: 'submenu.component.html',
+    animations: [
+        trigger('slideInOut', [
+            state('in', style({ height: '*', opacity: 0 })),
+            transition(':leave', [
+              style({ height: '*', opacity: 0.2 }),
+              group([
+                animate(300, style({ height: 0 })),
+                animate('200ms ease-out', style({ opacity: 0 }))
+              ])
+            ]),
+            transition(':enter', [
+              style({ height: '0', opacity: 0 }),
+              group([
+                animate(200, style({ height: '*' })),
+                animate('400ms ease-out', style({ opacity: 1 }))
+              ])
+            ])
+          ]),
+          trigger('isExpanded', [
+            state('no', style({ transform: 'rotate(-90deg)' })),
+            state('yes', style({ transform: 'rotate(0deg)', })),
+            transition('no => yes',
+              animate(300)
+            ),
+            transition('yes => no',
+              animate(300)
+            )
+        ]
+    )
+    ]
 })
 
 export class SubmenuComponent implements OnInit {
